514833174f20e3a31380ce496332b258ec21ba03
Tan Yi Qing <yiqingtan99@gmail.com>
2022-07-28 13:22:51 +0800

    second way of writing cout

--- Cpp_Statements_and_Functions/main.cpp
+++ Cpp_Statements_and_Functions/main.cpp
@@ -12,18 +12,23 @@ int main()
     int secondNum{4};
 
     std::cout << "First Number: " << firstNum << std::endl;
+    // cout << "First Number: " << firstNum << endl;
     std::cout << "Second Number: " << secondNum << std::endl;
+    // cout << "Second Number: " << secondNum << endl;
 
     //First Way
     int sum1 = firstNum + secondNum;
     std::cout << "Sum Number: " << sum1 << std::endl;
+    // cout << "Sum Number: " << sum1 << endl;
 
     //Second Way
     int sum2 = addNumbers(20, 120);
     std::cout << "Sum Number: " << sum2 << std::endl;
+    // cout << "Sum Number: " << sum2 << endl;
 
     //Third Way
     std::cout << "Sum Number: " << addNumbers(firstNum, secondNum) << std::endl;
+    // cout << "Sum Number: " << addNumbers(firstNum, secondNum) << endl;
 
     return 0;
 }
