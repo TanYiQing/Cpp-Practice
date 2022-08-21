865d01972092af12c9c5619487626b425120c7d1
Tan Yi Qing <yiqingtan99@gmail.com>
2022-08-21 14:38:26 +0800

    closing on private inheritance

---
+++ Cpp_Inheritance/Cpp_Closing_on_Private_Inheritance/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "person.h"
+#include "engineer.h"
+#include "civilengineer.h"
+
+int main()
+{
+    Engineer engineer1;
+    CivilEngineer cengineer1;
+
+    std::cout << "Done" << std::endl;
+    return 0;
+}
