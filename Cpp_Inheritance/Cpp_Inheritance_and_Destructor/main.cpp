6466a5255ba52f7797a9bb20ec01e777c5dfcaa9
Tan Yi Qing <yiqingtan99@gmail.com>
2022-08-23 00:05:31 +0800

    inheritance and destructor

---
+++ Cpp_Inheritance/Cpp_Inheritance_and_Destructor/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "person.h"
+#include "engineer.h"
+#include "civilengineer.h"
+
+int main()
+{
+
+	CivilEngineer cvl1("Daniel Gray", 41, "Green Sky Oh Blue 33St#75", 12, "Road");
+
+	return 0;
+}
