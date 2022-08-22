a066b5881fd9a176aff3871430f48effb7fead54
Tan Yi Qing <yiqingtan99@gmail.com>
2022-08-22 23:16:23 +0800

    Inheriting Base Constructor

---
+++ Cpp_Inheritance/Cpp_Inheriting_Base_Constructor/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "person.h"
+#include "engineer.h"
+
+int main()
+{
+
+	Engineer cvl1("Daniel Gray", 41, "Green Sky Oh Blue 33St#75");
+	std::cout << "Engineer 1: " << cvl1 << std::endl;
+
+	return 0;
+}
